cb6606526719e3b6717f0dde93f86b14633a52c3
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-09-21 21:51:40 +0530

    Contextandlocal

---
+++ todoContextLocal/src/Components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { useTodo } from '../Contexts';
+
+function TodoForm() {
+    const[todo,settodo]=useState("")
+    const {addtodo} = useTodo()
+    const add = (e) =>{
+      e.preventDefault()
+      if(!todo) return
+      addtodo({todo, completed:false})
+      settodo("")
+    }
+
+    return (
+        <form onSubmit={add} className="flex">
+            <input
+                value={todo}
+                onChange={(e)=>settodo(e.target.value)}
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+            />
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
+
